5bc6fde3116a33eb29d88e043a86f805c31746f4
Daniel Gallo <daniel.gallo@sencha.com>
2014-03-23 15:56:17 +0000

    Update Config.php

--- PHP Web Services/Config.php
+++ PHP Web Services/Config.php
@@ -3,6 +3,6 @@
 header('Content-Type: application/json');
 
 $method = $_SERVER["REQUEST_METHOD"];
-$con = new PDO("mysql:host=127.0.0.1;port=8889;dbname=customers", "root", "root");
+$con = new PDO("mysql:host=127.0.0.1;port=8889;dbname=customers", "user_name", "password");
 
 ?>
